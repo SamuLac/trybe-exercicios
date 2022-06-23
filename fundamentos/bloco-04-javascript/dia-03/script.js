a22c618c25e5dc3d6d6ce6da0c52ecd5383508c1
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-23 15:55:00 -0300

    adicionando exercicio 2

--- fundamentos/bloco-04-javascript/dia-03/script.js
+++ fundamentos/bloco-04-javascript/dia-03/script.js
@@ -4,11 +4,27 @@ Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.*/
 
 //RESPOSTA
 
-let fatorial = 10;
+/*let fatorial = 10;
 let resultado = 1;
 
 for (let i = fatorial; i > 1; i-=1) {
     resultado = resultado * i;
 }
 
-console.log(resultado);
+console.log(resultado);*/
+
+
+
+
+/*Exercício 2
+2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.*/
+
+//RESPOSTA
+
+let word = 'trybe';
+let resultado = "";
+for(let i = word.length - 1; i >= 0;i-=1){
+    resultado += word[i];
+}
+
+console.log(word,resultado);
