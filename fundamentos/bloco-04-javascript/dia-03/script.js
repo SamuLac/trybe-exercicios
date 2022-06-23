bf7b259dcc30780ebc5602868289076465910fcb
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-23 18:29:10 -0300

    adicionando exercicio bonus 2

--- fundamentos/bloco-04-javascript/dia-03/script.js
+++ fundamentos/bloco-04-javascript/dia-03/script.js
@@ -98,7 +98,7 @@ Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, da
 
 //RESPOSTA
 
-let n =5;
+/*let n =5;
 let linha = "";
 for(i=0;i<n;i+=1){
 
@@ -107,4 +107,19 @@ for(i=0;i<n;i+=1){
     }
     console.log(linha);
     linha="";
+}*/
+
+
+
+
+/*Exercício Bonus 2
+Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.*/
+
+//RESPOSTA
+
+let n = 5;
+let linha = "";
+for(i=0;i<n;i+=1){
+    linha +="*";
+    console.log(linha);
 }
