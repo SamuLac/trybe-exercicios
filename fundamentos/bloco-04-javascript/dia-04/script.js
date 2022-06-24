b599fc1b8a50d35a0959099d3fb5e8a9609711ab
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-24 16:18:24 -0300

    adicionando exercicio 6

--- fundamentos/bloco-04-javascript/dia-04/script.js
+++ fundamentos/bloco-04-javascript/dia-04/script.js
@@ -76,7 +76,9 @@ for (let key in info) {
 /*Exercício 5
 Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.*/
 
-let info = {
+//RESPOSTA
+
+/*let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -96,4 +98,40 @@ for (let key in info && info2) {
   }else {
     console.log(info[key] + ' e ' + info2[key]);
   }
-}
+}*/
+
+
+
+
+/*Exércício 6
+Usando o objeto abaixo, faça os exercícios a seguir:
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".*/
+
+//RESPOSTA
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+let favorito = leitor.livrosFavoritos[0];
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " +"'"+favorito['titulo']+"'");
