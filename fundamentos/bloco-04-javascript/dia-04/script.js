fd6cbcd919b0b1bd3a75e94cd13c9ce1353c00ee
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-24 15:51:16 -0300

    adicionando exercicio 1

---
+++ fundamentos/bloco-04-javascript/dia-04/script.js
@@ -0,0 +1,18 @@
+/*Exercício 1
+Usando o objeto abaixo, faça os exercícios a seguir:
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+}; 
+1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.*/
+
+//RESPOSTA
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  }; 
+
+  console.log("Bem-vinda, " + info.personagem);
