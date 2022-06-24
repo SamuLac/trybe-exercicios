abe188a0e72f4480cdc0f2d679dfee26334a045b
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-24 16:23:11 -0300

    adicionando exerciocio 7

--- fundamentos/bloco-04-javascript/dia-04/script.js
+++ fundamentos/bloco-04-javascript/dia-04/script.js
@@ -121,7 +121,7 @@ let leitor = {
 
 //RESPOSTA
 
-let leitor = {
+/*let leitor = {
   nome: 'Julia',
   sobrenome: 'Pessoa',
   idade: 21,
@@ -134,4 +134,38 @@ let leitor = {
   ],
 };
 let favorito = leitor.livrosFavoritos[0];
-console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " +"'"+favorito['titulo']+"'");
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " +"'"+favorito['titulo']+"'");*/
+
+
+
+
+/*Exercício 7
+7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+{
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+}*/
+
+//RESPOSTA
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
+console.log(leitor);
