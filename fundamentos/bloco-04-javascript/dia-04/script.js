37bc389a13697b2a10cd1a7937e30022564e256e
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-24 16:10:48 -0300

    adicionando exercicio 5

--- fundamentos/bloco-04-javascript/dia-04/script.js
+++ fundamentos/bloco-04-javascript/dia-04/script.js
@@ -60,7 +60,7 @@ Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.*/
 
 //RESPOSTA
 
-let info = {
+/*let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -68,4 +68,32 @@ let info = {
 
 for (let key in info) {
   console.log(info[key]);
+}*/
+
+
+
+
+/*Exercício 5
+Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.*/
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+}; 
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}; 
+
+for (let key in info && info2) {
+  if (key === 'recorrente' && info[key]==='Sim' && info2[key]==='Sim') {
+    console.log("Ambos recorrentes");
+  }else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
 }
