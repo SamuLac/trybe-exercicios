1418e0e7fa6033df81e6783b0c13c9882a8f3879
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-28 18:18:02 -0300

    exercicio 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01/script.js
@@ -0,0 +1,23 @@
+/*
+Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+- document.getElementById()
+- document.getElementsByClassName()
+- document.getElementsByTagName()
+1. Crie uma função que mude o texto na tag <p>-----</p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+3. Crie uma função que mude a cor do quadrado vermelho para branco.
+4. Crie uma função que corrija o texto da tag <h1>.
+5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+*/
+
+
+//EXERCICIO 1
+function mudaParagrafo(text, i) {
+    let aux = document.getElementsByTagName("p");
+    aux[i].innerText = text;
+}
+
+texto = 'Olá sou o Samuel em 2024 possuo 23 anos sou desenvolvedor web full stack me formei pela trybe e trabalho a 1 ano aqui na empresa x'
+
+mudaParagrafo(texto, 1);
