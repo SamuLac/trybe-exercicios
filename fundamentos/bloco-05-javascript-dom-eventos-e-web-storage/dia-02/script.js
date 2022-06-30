0d09f53d35ef85f70984dc3b572b9ae1abc0a2f3
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-06-30 13:37:49 -0300

    exercicio 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02/script.js
@@ -1,7 +1,14 @@
+//Exercício 1
+
 let titleContent = 'Exercício 5.2 - JavaScript DOM';
 let bodyPosition = document.lastChild.lastChild;
 let title = document.createElement('h1');
 title.innerText = titleContent;
-console.log(bodyPosition);
 bodyPosition.appendChild(title);
 
+//Exercício 2
+
+let main = document.createElement('main');
+main.className = 'main-content';
+bodyPosition.appendChild(main);
+console.log(main);
