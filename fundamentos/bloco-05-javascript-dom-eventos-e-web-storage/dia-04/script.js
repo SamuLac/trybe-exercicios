67663cc9a896f393ec82e92be96eff52469ce5bd
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2022-07-01 15:45:09 -0300

    cor da fonte

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04/script.js
@@ -1,4 +1,11 @@
+//Set Configs
 document.body.style.backgroundColor = localStorage.getItem('BackGround');
+document.getElementById('texto').style.color = localStorage.getItem('FontColor');
+
+
+//Functions
+
+//BackGround
 let buttonCor = document.getElementById('button-color');
 buttonCor.addEventListener('click',escolheCor);
 
@@ -7,3 +14,13 @@ function escolheCor(){
     localStorage.setItem('BackGround', cor.value);
 }
 
+
+//Text-Color
+let buttonCorFonte = document.getElementById('button-cor-fonte');
+buttonCorFonte.addEventListener('click',corFonte);
+
+function corFonte(){
+    let p = document.getElementById('texto');
+    p.style.color = cordefonte.value;
+    localStorage.setItem('FontColor', cordefonte.value);
+}
